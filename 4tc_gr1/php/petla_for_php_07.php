51bb6afac5b19f66929dd45480170204a867d405
git.edu <144489666+Punc5@users.noreply.github.com>
2025-11-20 11:45:39 +0100

    updated: petla_for_php_07.php

--- 4tc_gr1/php/petla_for_php_07.php
+++ 4tc_gr1/php/petla_for_php_07.php
@@ -3,30 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pętla for w PHP - część 7.</title>
+    <title>Pętla for w PHP - część 6.</title>
 </head>
 <body> 
     <?php
-    // Napisz skrypt, w którym za pomocą pętli for obliczysz wartość wyrażenia: 2 * 2,5 * 3 * 3,5 * 4 * 4,5 / -3 * (-0,3) * (-0,03) * (-0,003) * ...
-    $iloczyn_1 = 1;
-    $iloczyn_2 = 1;
+    // Napisz skrypt, w którym za pomocą pętli for obliczysz wartość wyrażenia: 1 + 2 + 3 + 4 + 5 + ... / 2 * 6 * 10 * 14 * 18 * ...
+    $suma = 0;
+    $iloczyn = 1;
     $n = rand(2, 10);
 
-    $p = 2;
-    $q = -3;
+    $p = 1;
+    $q = 2;
 
     for ($i = 1; $i <= $n; $i++) {
-        $iloczyn_1 *= $p;
-        $p += 0.5;
+        $suma += $p;
+        $p++;
     }
 
     for ($i = 1; $i <= $n; $i++) {
-        $iloczyn_2 *= $q;
-        $q *= 0.1;
-        $q++;
+        $iloczyn *= $q;
+        $q += 4;
     }
 
-    echo "Wartość = " .$iloczyn_1 / $iloczyn_2; 
+    echo "Wartość = " . ($suma / $iloczyn);
     ?>
 </body>
 </html>
