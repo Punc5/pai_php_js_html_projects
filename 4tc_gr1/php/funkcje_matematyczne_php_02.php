616b1a6f00504df1c67f840745b2c5226d41df05
git.edu <144489666+Punc5@users.noreply.github.com>
2025-11-20 11:34:44 +0100

    updated:  funkcje_matematyczne_php_02.php

--- 4tc_gr1/php/funkcje_matematyczne_php_02.php
+++ 4tc_gr1/php/funkcje_matematyczne_php_02.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,24 +7,41 @@
 </head>
 <body>
     <?php
-        // Napisz skrypt, w którym dla wylosowanej wartości z przedziału od 1 do 10 wyznaczy rozwiązanie poniższych wyrażeń:
+        // Wylosowanie liczby z przedziału od 1 do 10
         $x = rand(1, 10);
 
         // a)
-        $dzialanie_1 = (sqrt(pow($x + 1, 2) + 3) - 2 * $x) / (3 * pow($x, 2) * pow(pow($x, 2) + 1, 3) + (2 / 3) * $x + 1);
-        echo "Wynik działania 1. = " .$dzialanie_1. "<br>";
+        $dzialanie_1 = (sqrt(pow($x + 1, 2) + 3) - 2 * $x) / 
+                        (3 * pow($x, 2) * pow(pow($x, 2) + 1, 3) + (2 / 3) * $x + 1);
+        echo "Wynik działania 1. = " . $dzialanie_1 . "<br>";
 
         // b)
-        $dzialanie_2 = ((sin($x) / (2 * $x)) + (8 * (pow($x, 3) - 1)) / (pow(2 * $x + 1, 8))) / ((abs(pow($x, 2) + 1)) - ((3 * pow($x, 3) - 1) / (2 * pow($x, 2) + 2)));
-        echo "Wynik działania 2. = " .$dzialanie_2. "<br>";
+        $denominator_2 = (abs(pow($x, 2) + 1)) - ((3 * pow($x, 3) - 1) / (2 * pow($x, 2) + 2));
+        if ($denominator_2 != 0) {
+            $dzialanie_2 = ((sin($x) / (2 * $x)) + (8 * (pow($x, 3) - 1)) / (pow(2 * $x + 1, 8))) / $denominator_2;
+            echo "Wynik działania 2. = " . $dzialanie_2 . "<br>";
+        } else {
+            echo "Działanie 2: Dzielenie przez zero!<br>";
+        }
 
         // c)
-        $dzialanie_3 = (sqrt(2 * sqrt(pow(pow($x, 2) - 1, 2))) + 2 * pow($x, 2)) / (2 * abs(pow($x, 2) - 1)) - ((8 * (pow($x, 2) - 1)) / (3 * $x + 2));
-        echo "Wynik działania 3. = " .$dzialanie_3. "<br>";
+        $denominator_3 = 2 * abs(pow($x, 2) - 1);
+        if ($denominator_3 != 0 && (3 * $x + 2) != 0) {
+            $dzialanie_3 = (sqrt(2 * sqrt(pow(pow($x, 2) - 1, 2))) + 2 * pow($x, 2)) / 
+                            $denominator_3 - ((8 * (pow($x, 2) - 1)) / (3 * $x + 2));
+            echo "Wynik działania 3. = " . $dzialanie_3 . "<br>";
+        } else {
+            echo "Działanie 3: Dzielenie przez zero!<br>";
+        }
 
         // d)
-        $dzialanie_4 = (5 * pow($x, 2) - pow(cos($x), 2) - 3 * $x) / ((5 * pow($x, 2) - 1) / (pow(3 * $x, $x - 1)) + 2 * pow($x, 2)) - 2 * pow($x, 3);
-        echo "Wynik działania 4. = " .$dzialanie_4. "<br>";
+        $denominator_4 = ((5 * pow($x, 2) - 1) / (pow(3 * $x, $x - 1)) + 2 * pow($x, 2));
+        if ($denominator_4 != 0) {
+            $dzialanie_4 = (5 * pow($x, 2) - pow(cos($x), 2) - 3 * $x) / $denominator_4 - 2 * pow($x, 3);
+            echo "Wynik działania 4. = " . $dzialanie_4 . "<br>";
+        } else {
+            echo "Działanie 4: Dzielenie przez zero!<br>";
+        }
     ?>
 </body>
 </html>
