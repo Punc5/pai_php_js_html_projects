1b8d5228c09fc04478fecfab063044bcbd5d04ce
git.edu <144489666+Punc5@users.noreply.github.com>
2025-11-20 11:31:25 +0100

    Update funkcje_matematyczne_php_01.php

--- 4tc_gr1/php/funkcje_matematyczne_php_01.php
+++ 4tc_gr1/php/funkcje_matematyczne_php_01.php
@@ -16,29 +16,17 @@
         $zmienna2 = true;
         $zmienna3 = -8;
 
-        echo "wartość zmiennej = " .$zmienna.",zmienna2 =" .$zmienna1. ",zmienna3 =" .$zmienna2. ", zmienna4 =" .$zmienna3. "<br>";
-        echo "wartość zmiennej = $zmienna ,zmienna2 = $zmienna1 ,zmienna3 = $zmienna2 , zmienna4 = $zmienna3 <br>";
-        echo "wartość zmiennej = $zmienna ,zmienna2 = $zmienna1 ,zmienna3 = $zmienna2 , zmienna4 = $zmienna3 <br>";
+        echo "wartość zmiennej = " .$zmienna.", zmienna2 =" .$zmienna1. ", zmienna3 =" .$zmienna2. ", zmienna4 =" .$zmienna3. "<br>";
+        echo "wartość zmiennej = $zmienna , zmienna2 = $zmienna1 , zmienna3 = $zmienna2 , zmienna4 = $zmienna3 <br>";
 
-        // Pierwszy sposób na komentarz
-        /* Drugi sposób na komentarz */
-
-        $zmienna_1 = 5.5; // liczba zmienno-przecinkowa
-        $zmienna_2 = -8; // liczba całkowita
-        $zmienna_3 = true; // logika (true-false)
-        $zmienna_4 = "tekst"; // tekst
-
-        // round(x) - zaokraglenie x
-        // rand(x, y) - losowa cyfra ze zbioru x i y
-        // Napisz skrypt, w którym dla wylosowanej wartości z przedziału od 1 do 10 wyznaczy rozwiązanie poniższych wyrażeń:
         $x = rand(1,10);
 
         // a)
-        $dzialanie_1 = (pow($x,2) - sqrt(pow($x+2,3))) / (2pow($x+2,2)-3$x);
+        $dzialanie_1 = (pow($x,2) - sqrt(pow($x+2,3))) / (2*pow($x+2,2) - 3*$x);
         echo "Wynik działania 1. = " .$dzialanie_1. "<br>";
 
         // b)
-        $dzialanie_2 = (abs(pow($x,2)-1)-pow(sin($x),2)) / ((2*$x+3)/(2pow($x,3)) - 3($x+1));
+        $dzialanie_2 = (abs(pow($x,2)-1)-pow(sin($x),2)) / ((2*$x+3)/(2*pow($x,3)) - 3*($x+1));
         echo "Wynik działania 2. = " .$dzialanie_2. "<br>";
     ?>
 </body>
